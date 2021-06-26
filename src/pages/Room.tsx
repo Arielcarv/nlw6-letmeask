2a65322c10a3ac8b2624eaaa74edd3097b5dfaac
Ariel Carvalho <arielcarvnasc@gmailcom>
2021-06-26 17:02:27 -0300

    Aula3

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,34 +1,131 @@
 import logoImg from "../assets/images/logo.svg";
+import { FormEvent, useEffect, useState } from "react";
+import { useParams } from "react-router";
 import { Button } from "../components/Button";
 import { RoomCode } from "../components/RoomCode";
 import "../styles/room.scss";
+import { useAuth } from "../hooks/useAuth";
+import { database } from "../services/firebase";
+
+type RoomParameters = {
+  id: string;
+};
+type FirebaseQuestions = Record<
+  string,
+  {
+    author: {
+      name: string;
+      avatar: string;
+    };
+    content: string;
+    isAnswered: boolean;
+    isHighlighted: boolean;
+  }
+>;
+
+type Question = {
+  id: string;
+  author: {
+    name: string;
+    avatar: string;
+  };
+  content: string;
+  isAnswered: boolean;
+  isHighlighted: boolean;
+};
 
 export function Room() {
+  const { user } = useAuth();
+  const roomParameters = useParams<RoomParameters>();
+  const roomId = roomParameters.id;
+  const [newQuestion, setNewQuestion] = useState("");
+  const [questions, setQuestions] = useState<Question[]>([]);
+  const [title, setTitle] = useState("");
+
+  useEffect(() => {
+    const roomRef = database.ref(`rooms/${roomId}`);
+
+    roomRef.on("value", (room) => {
+      const databaseRoom = room.val();
+      const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
+      const parsedQuestions = Object.entries(firebaseQuestions).map(
+        ([key, value]) => {
+          return {
+            id: key,
+            content: value.content,
+            author: value.author,
+            isHighlighted: value.isHighlighted,
+            isAnswered: value.isAnswered,
+          };
+        }
+      );
+
+      setTitle(databaseRoom.title);
+      setQuestions(parsedQuestions);
+    });
+  }, [roomId]);
+
+  async function handleCreateNewQuestion(event: FormEvent) {
+    event.preventDefault();
+    if (newQuestion.trim() === "") {
+      return;
+    }
+    if (!user) {
+      throw new Error("You must be logged in");
+    }
+
+    const question = {
+      content: newQuestion,
+      author: {
+        name: user.name,
+        avatar: user.avatar,
+      },
+      isHighlighted: false,
+      isAnswered: false,
+    };
+    await database.ref(`rooms/${roomParameters.id}/questions`).push(question);
+    setNewQuestion("");
+  }
   return (
     <div id="page-room">
       <header>
         <div className="content">
           <img src={logoImg} alt="LetMeAsk Logo" />
-          {/* <div>codigo</div> */}
-          <RoomCode/>
+          <RoomCode code={roomParameters.id} />
         </div>
       </header>
 
       <main>
         <div className="room-title">
-          <h1>Sala React</h1>
-          <span>4 questions</span>
+          <h1>Room {title}</h1>
+          {questions.length > 0 && <span>{questions.length} question(s)</span>}
         </div>
 
-        <form>
-          <textarea placeholder="What do you want to ask?" />
+        <form onSubmit={handleCreateNewQuestion}>
+          <textarea
+            placeholder="What do you want to ask?"
+            onChange={(event) => setNewQuestion(event.target.value)}
+            value={newQuestion}
+          />
           <div className="form-footer">
-            <span>
-              <button>Login</button> to send a question.
-            </span>
-            <Button type="submit">Send Question</Button>
+            {user ? (
+              <div className="user-info">
+                <img src={user.avatar} alt={user.name} />
+                <span>{user.name}</span>
+              </div>
+            ) : (
+              <span>
+                <button>Login</button> to send a question.
+              </span>
+            )}
+
+            <Button type="submit" disabled={!user}>
+              Send Question
+            </Button>
           </div>
         </form>
+
+        {JSON.stringify(questions)}
       </main>
     </div>
   );
